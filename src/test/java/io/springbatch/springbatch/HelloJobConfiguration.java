500587be8244ad7c9cfa3810620adaf6afc36591
0JUUU <clleo97@naver.com>
2022-03-23 11:50:39 +0900

    feat: :sparkles: Step 생성
    - tasklet: Step 안에서 단일 태스크로 수행되는 로직 구
    - tasklet(Tasklet) : Tasklet - 인터페이스

--- src/test/java/io/springbatch/springbatch/HelloJobConfiguration.java
+++ src/test/java/io/springbatch/springbatch/HelloJobConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,6 +29,11 @@ public class HelloJobConfiguration {
 
     @Bean
     public Step helloStep() {
-        return null;
+        return stepBuilderFactory.get("helloStep")
+                                 .tasklet(((contribution, chunkContext) -> {
+                                     System.out.println("Hello Spring Batch");
+                                     return RepeatStatus.FINISHED;
+                                 }))
+                                 .build();
     }
 }
