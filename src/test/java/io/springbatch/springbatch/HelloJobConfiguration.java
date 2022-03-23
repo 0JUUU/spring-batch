320b7142baa18d58515a76fbd6a1ae3e577dd923
0JUUU <clleo97@naver.com>
2022-03-23 11:39:46 +0900

    feat: :sparkles: 유틸성 빌더 팩토리 의존성 주입
    - 스프링배치가 Job을 편리하게 구성할 수 있도록 제공하는 Util클래스 선언 & 의존성 주입
    1. JobBuilderFactory
    2. StepBuilderFactory

--- src/test/java/io/springbatch/springbatch/HelloJobConfiguration.java
+++ src/test/java/io/springbatch/springbatch/HelloJobConfiguration.java
@@ -1,7 +1,18 @@
 package io.springbatch.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public HelloJobConfiguration(JobBuilderFactory jobBuilderFactory,
+                                 StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
 }
