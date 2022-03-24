991225eb96cc469bed3be471a448ea3121f728a4
0JUUU <clleo97@naver.com>
2022-03-24 17:57:11 +0900

    feat: :sparkles: 비동기적 실행 코드 추가 #2
    - 인터페이스 사용 시에는 인터페이스에 사용할 메서드가 있나 확인하자
    - 없다면, 그것을 구현한 구현 클래스를 직접 사용하자

--- src/main/java/io/springbatch/springbatch/JobLauncherController.java
+++ src/main/java/io/springbatch/springbatch/JobLauncherController.java
@@ -7,10 +7,13 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,9 @@ public class JobLauncherController {
     @Autowired
     private JobLauncher jobLauncher;
 
+    @Autowired
+    private BasicBatchConfigurer basicBatchConfigurer;
+
     @PostMapping("/batch")
     public String launch(@RequestBody Member member) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
         JobParameters jobParameters = new JobParametersBuilder()
@@ -31,6 +37,10 @@ public class JobLauncherController {
             .addDate("date", new Date())
             .toJobParameters();
 
+        SimpleJobLauncher jobLauncher = (SimpleJobLauncher) basicBatchConfigurer.getJobLauncher();
+        // 밑에 문장은 예외 발생 -> typeCasting 불가 (왜? 이미 인터페이스로 선언했던 친구이기에 JobLauncher에서는 갖고 있지 않은 setTaskExecutor를 호출할 수 없음!
+//        SimpleJobLauncher jobLauncher = (SimpleJobLauncher) this.jobLauncher;
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
         jobLauncher.run(job, jobParameters);
 
         return "batch completed";
