8cffec13329ed6f2f118fdc6247e0ac544d8c2cd
0JUUU <clleo97@naver.com>
2022-03-28 15:16:31 +0900

    feat: :sparkles: 기본적인 로직 구현 #10

---
+++ src/main/java/io/springbatch/springbatch/SimpleFlowConfiguration.java
@@ -0,0 +1,54 @@
+package io.springbatch.springbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class SimpleFlowConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+            .start(step3())
+            .next(step3())
+            .build();
+    }
+    
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     System.out.println(">> step1 has executed");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     System.out.println(">> step2 has executed");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     System.out.println(">> step3 has executed");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+}
