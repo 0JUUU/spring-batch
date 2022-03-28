166565746e5d1c2a492973f266741f92db713fa5
0JUUU <clleo97@naver.com>
2022-03-28 11:00:18 +0900

    feat: :sparkles: JobStep에 대한 전체적인 내용 확인 #9

---
+++ src/main/java/io/springbatch/springbatch/JobStepConfiguration.java
@@ -0,0 +1,100 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
+import org.springframework.batch.core.step.job.JobParametersExtractor;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.*;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Configuration
+public class JobStepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job parentJob() {
+        return this.jobBuilderFactory.get("parentJob")
+                                     .start(jobStep(null))
+                                     .next(step2())
+                                     .build();
+    }
+
+    @Bean
+    public Step jobStep(JobLauncher jobLauncher) {
+        return stepBuilderFactory.get("jobStep")
+            .job(childJob())
+            .launcher(jobLauncher)
+            .parametersExtractor(jobParametersExtractor())
+            .listener(new StepExecutionListener() {
+                @Override
+                public void beforeStep(StepExecution stepExecution) {
+                    stepExecution.getExecutionContext().putString("name", "user1");
+                }
+
+                @Override
+                public ExitStatus afterStep(StepExecution stepExecution) {
+                    return null;
+                }
+            })
+            .build();
+    }
+
+    @Bean
+    public Job childJob() {
+        return this.jobBuilderFactory.get("childJob")
+                                     .start(step1())
+                                     .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                        throw new RuntimeException("step1 was failed");
+                                         return RepeatStatus.FINISHED;
+                                     }
+                                 })
+                                 .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                                         throw new RuntimeException("step2 was failed");
+//                        return RepeatStatus.FINISHED;
+                                     }
+                                 })
+                                 .build();
+    }
+
+
+    private DefaultJobParametersExtractor jobParametersExtractor() {
+        DefaultJobParametersExtractor extractor = new DefaultJobParametersExtractor();
+        extractor.setKeys(new String[]{"name"});
+        return extractor;
+    }
+}
