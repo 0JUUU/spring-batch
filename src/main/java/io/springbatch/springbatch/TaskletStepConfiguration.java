896fa76411fb745fd46ebb9a24e7fe04deda727f
0JUUU <clleo97@naver.com>
2022-03-28 09:47:12 +0900

    feat: :sparkles: Tasklet 기반의 Step 관련 코드 #9
    - .tasklet(new Tasklet())

---
+++ src/main/java/io/springbatch/springbatch/TaskletStepConfiguration.java
@@ -0,0 +1,45 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.item.*;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Configuration
+public class TaskletStepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("batchJob")
+                                     .start(taskStep())
+                                     .build();
+    }
+
+    @Bean
+    public Step taskStep() {
+        return stepBuilderFactory.get("taskStep")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     System.out.println("step1 has executed");
+                                     return RepeatStatus.FINISHED;
+                                 })
+                                 .build();
+    }
+}
