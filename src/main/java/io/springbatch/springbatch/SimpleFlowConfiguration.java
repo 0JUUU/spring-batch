b317cf67f6cb81e972c30b33da9aa3adf603cf84
0JUUU <clleo97@naver.com>
2022-03-28 15:25:10 +0900

    feat: :sparkles: Flow 개념, 흐름 정리 #10

--- src/main/java/io/springbatch/springbatch/SimpleFlowConfiguration.java
+++ src/main/java/io/springbatch/springbatch/SimpleFlowConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,18 +15,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Configuration
 public class SimpleFlowConfiguration {
-
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    // FlowJob 구성
     @Bean
     public Job job() {
         return jobBuilderFactory.get("batchJob")
-            .start(step3())
-            .next(step3())
-            .build();
+                                .start(flow())
+                                // SimpleFlow 구성
+                                .next(step3())
+                                .end()
+                                .build();
     }
-    
+
+    @Bean
+    public Flow flow() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+        flowBuilder.start(step1())
+                   .next(step2())
+                   .end();
+        return flowBuilder.build();
+    }
+
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
