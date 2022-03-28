786daa2c0f80ae61427ab75d7bce406e7be2b5d5
0JUUU <clleo97@naver.com>
2022-03-28 11:40:37 +0900

    feat: :sparkles: FlowJob 구성 #10
    - step1이 성공하면 step2로
    - step1이 실패하면 step3로

---
+++ src/main/java/io/springbatch/springbatch/FlowJobConfiguration.java
@@ -0,0 +1,80 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.*;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Configuration
+public class FlowJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("batchJob")
+                                .start(step1())
+                                .on("COMPLETED").to(step2())
+                                .from(step1())
+                                .on("FAILED").to(step3())
+                                .end()
+                                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                                         System.out.println("step1 has executed");
+//                        throw new RuntimeException("");
+                                         return RepeatStatus.FINISHED;
+                                     }
+                                 }).build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                                 .tasklet((contribution, chunkContext) -> {
+                                     System.out.println("step2 has executed");
+                                     return RepeatStatus.FINISHED;
+                                 }).build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                                 .tasklet(new Tasklet() {
+                                     @Override
+                                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                                         System.out.println("step3 has executed");
+                                         return RepeatStatus.FINISHED;
+                                     }
+                                 }).build();
+    }
+
+
+}
