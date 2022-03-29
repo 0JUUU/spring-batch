4cac9fa27ee61c32d90c358b7a88e6fca7392503
0JUUU <clleo97@naver.com>
2022-03-30 01:02:49 +0900

    feat: :sparkles: xml 파일과 매핑할 클래스 구현 #12

---
+++ src/main/java/io/springbatch/springbatch/Customer.java
@@ -0,0 +1,12 @@
+package io.springbatch.springbatch;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private final long id;
+    private final String name;
+    private final int age;
+
+}
