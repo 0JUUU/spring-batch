5d02270dee1d64a2e28e5c24bd02082eaf55d1f6
0JUUU <clleo97@naver.com>
2022-03-30 02:57:35 +0900

    feat: :sparkles: column명 수정 #12
    - location -> username

--- src/main/java/io/springbatch/springbatch/Customer.java
+++ src/main/java/io/springbatch/springbatch/Customer.java
@@ -16,7 +16,7 @@ public class Customer {
     @Id
     @GeneratedValue
     private Long id;
-    private String location;
+    private String username;
     private int age;
 
     @OneToOne(mappedBy = "customer")
