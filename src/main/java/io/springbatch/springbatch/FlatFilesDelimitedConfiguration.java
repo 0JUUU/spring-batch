d3932567f7b3c849f8120cf4783b9efff52961bb
0JUUU <clleo97@naver.com>
2022-03-29 23:52:42 +0900

    feat: :sparkles: 기본적인 Job과 Step 구성 #12

---
+++ src/main/java/io/springbatch/springbatch/FlatFilesDelimitedConfiguration.java
@@ -0,0 +1,70 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class FlatFilesDelimitedConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+            .start(step1())
+            .next(step2())
+            .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+            .<String, String>chunk(3)
+            .reader(null)
+            .writer(new ItemWriter() {
+                @Override
+                public void write(List items) throws Exception {
+                    items.forEach(item -> System.out.println(item));
+                }
+            })
+            .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+            .tasklet((contribution, chunkContext) -> {
+                System.out.println("step2 has executed");
+                return RepeatStatus.FINISHED;
+            })
+            .build();
+    }
+}
