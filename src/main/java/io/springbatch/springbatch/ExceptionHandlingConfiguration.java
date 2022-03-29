3049cfaef99bee8ce61a7b11d941ae72f12292c3
0JUUU <clleo97@naver.com>
2022-03-30 00:56:26 +0900

    feat: :sparkles: parsing 예외 처리 관련 설정을 확인하기 위한 코드 #12

---
+++ src/main/java/io/springbatch/springbatch/ExceptionHandlingConfiguration.java
@@ -0,0 +1,74 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class ExceptionHandlingConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+            .start(step1())
+            .next(step2())
+            .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+            .<String, String>chunk(3)
+            .reader(itemReader())
+            .writer(new ItemWriter() {
+                @Override
+                public void write(List items) throws Exception {
+                    items.forEach(item -> System.out.println(item));
+                }
+            })
+            .build();
+    }
+
+    public FlatFileItemReader itemReader() {
+        return new FlatFileItemReaderBuilder<Customer>()
+            .name("flatFile")
+            .resource(new ClassPathResource("customer.txt"))
+            .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
+            .targetType(Customer.class)
+            .linesToSkip(1)
+            .fixedLength()
+            .strict(false)
+            .addColumns(new Range(1,5))
+            .addColumns(new Range(6,9))
+            .addColumns(new Range(10,11))
+            .names("name","year","age")
+            .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+            .tasklet((contribution, chunkContext) -> {
+                System.out.println("step2 has executed");
+                return RepeatStatus.FINISHED;
+            })
+            .build();
+    }
+}
+
