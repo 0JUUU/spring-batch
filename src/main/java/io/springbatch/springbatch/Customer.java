672b32f8ec099b643c84d04b1102b71bb9e3b682
0JUUU <clleo97@naver.com>
2022-03-30 04:33:51 +0900

    feat: :sparkles: 매핑할 Customer 클래스 구현 #13

---
+++ src/main/java/io/springbatch/springbatch/Customer.java
@@ -0,0 +1,16 @@
+package io.springbatch.springbatch;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+
+    private final long id;
+    private final String firstName;
+    private final String lastName;
+    private final Date birthdate;
+}
