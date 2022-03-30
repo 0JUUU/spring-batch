47af06ad0781ca3cd794e895a6078cbf254f86af
0JUUU <clleo97@naver.com>
2022-03-30 09:20:03 +0900

    feat: :sparkles: JdbcBatchItemWriter 동작 확인을 위한 코드 #13
    - 이전에 customer2라는 테이블 생성 (customer과 동일, 테이블명만 다름)
    - JdbcBatchConfiguration

---
+++ src/main/java/io/springbatch/springbatch/JdbcBatchConfiguration.java
@@ -0,0 +1,96 @@
+package io.springbatch.springbatch;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class JdbcBatchConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+    @Bean
+    public Job job() throws Exception {
+        return jobBuilderFactory.get("batchJob")
+            .incrementer(new RunIdIncrementer())
+            .start(step1())
+            .build();
+    }
+
+    @Bean
+    public Step step1() throws Exception {
+        return stepBuilderFactory.get("step1")
+            .<Customer, Customer>chunk(100)
+            .reader(customItemReader())
+            .writer(customItemWriter())
+            .build();
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Customer> customItemReader() {
+
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+
+        reader.setDataSource(this.dataSource);
+        reader.setFetchSize(100);
+        reader.setRowMapper(new CustomerRowMapper());
+
+        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
+//        queryProvider.setWhereClause("where firstname like :firstname");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+
+        sortKeys.put("id", Order.ASCENDING);
+        queryProvider.setSortKeys(sortKeys);
+        reader.setQueryProvider(queryProvider);
+
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("firstname", "A%");
+
+//        reader.setParameterValues(parameters);
+
+        return reader;
+    }
+
+    @Bean
+    public ItemWriter<? super Customer> customItemWriter() {
+        return new JdbcBatchItemWriterBuilder<Customer>()
+            .dataSource(dataSource)
+            .sql("insert into customer2 values(:id, :firstName, :lastName, :birthdate)")
+            .beanMapped()
+            .build();
+    }
+
+    /*
+    @Bean
+    public JdbcBatchItemWriter<Customer> customItemWriter() {
+        JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
+
+        itemWriter.setDataSource(this.dataSource);
+        itemWriter.setSql("insert into customer2 values (:id, :firstName, :lastName, :birthdate)");
+        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider());
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+    }
+    */
+}
