0637b42b9a28f3735610fc0b836f17fe4cab2e14
0JUUU <clleo97@naver.com>
2022-03-30 09:22:35 +0900

    feat: :sparkles: 매핑할 Customer 클래스와 라인과 객체를 매핑할 CustomerRowMapper 클래스 구현 #13

---
+++ src/main/java/io/springbatch/springbatch/Customer.java
@@ -0,0 +1,14 @@
+package io.springbatch.springbatch;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+}
