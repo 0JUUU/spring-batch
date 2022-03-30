1e605f9afe00ca7aedea447b894ef79670543b89
0JUUU <clleo97@naver.com>
2022-03-30 16:09:48 +0900

    feat: :sparkles: Skip 관련 예외 구현 #15
    - SkippableException
    - NoSkippableException

---
+++ src/main/java/io/springbatch/springbatch/NoSkippableException.java
@@ -0,0 +1,8 @@
+package io.springbatch.springbatch;
+
+public class NoSkippableException extends Throwable {
+
+    public NoSkippableException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/io/springbatch/springbatch/SkippableException.java
@@ -0,0 +1,8 @@
+package io.springbatch.springbatch;
+
+public class SkippableException extends Throwable {
+
+    public SkippableException(String s) {
+        super(s);
+    }
+}
