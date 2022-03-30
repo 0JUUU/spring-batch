7afc94d0815385486e1efdf011697c5eecff95e9
0JUUU <clleo97@naver.com>
2022-03-30 18:27:54 +0900

    feat: :sparkles: RetryItemProcessor2 내부에서 DefaultRetryState 추가 구현 #15
    - 결과 : 다시 Chunk 시작부터 실행

--- src/main/java/io/springbatch/springbatch/template/RetryItemProcessor2.java
+++ src/main/java/io/springbatch/springbatch/template/RetryItemProcessor2.java
@@ -3,9 +3,12 @@ package io.springbatch.springbatch.template;
 import io.springbatch.springbatch.RetryableException;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.classify.BinaryExceptionClassifier;
+import org.springframework.classify.Classifier;
 import org.springframework.retry.RecoveryCallback;
 import org.springframework.retry.RetryCallback;
 import org.springframework.retry.RetryContext;
+import org.springframework.retry.support.DefaultRetryState;
 import org.springframework.retry.support.RetryTemplate;
 
 public class RetryItemProcessor2 implements ItemProcessor<String, Customer> {
@@ -16,6 +19,8 @@ public class RetryItemProcessor2 implements ItemProcessor<String, Customer> {
 
     @Override
     public Customer process(String item) throws Exception {
+        Classifier<Throwable, Boolean> rollbackClassifier = new BinaryExceptionClassifier(true);
+
         Customer customer = retryTemplate.execute(new RetryCallback<Customer, RuntimeException>() {
             @Override
             public Customer doWithRetry(RetryContext context) throws RuntimeException {
@@ -30,7 +35,7 @@ public class RetryItemProcessor2 implements ItemProcessor<String, Customer> {
             public Customer recover(RetryContext context) throws Exception {
                 return new Customer(item);
             }
-        });
+        }, new DefaultRetryState(item, rollbackClassifier));
         return customer;
     }
 }
