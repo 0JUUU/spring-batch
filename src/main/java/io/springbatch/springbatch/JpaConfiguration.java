508bb1ba4b4b12659e110eb62118108e0f9c8933
0JUUU <clleo97@naver.com>
2022-03-30 09:49:05 +0900

    feat: :sparkles: JpaItemWriter 동작을 확인하기 위한 코드 추가 #13
    - JpaConfiguration
    - 결과 : 조건에 맞는 Customer가 엔티티인 Customer2에 추가

---
+++ src/main/java/io/springbatch/springbatch/JpaConfiguration.java
@@ -0,0 +1,89 @@
+package io.springbatch.springbatch;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class JpaConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public Job job() throws Exception {
+        return jobBuilderFactory.get("batchJob")
+            .incrementer(new RunIdIncrementer())
+            .start(step1())
+            .build();
+    }
+
+    @Bean
+    public Step step1() throws Exception {
+        return stepBuilderFactory.get("step1")
+            .<Customer, Customer2>chunk(10)
+            .reader(customItemReader())
+            .processor(customItemProcessor())
+            .writer(customItemWriter())
+            .build();
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Customer> customItemReader() {
+
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+
+        reader.setDataSource(this.dataSource);
+        reader.setFetchSize(10);
+        reader.setRowMapper(new CustomerRowMapper());
+
+        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
+        queryProvider.setWhereClause("where firstname like :firstname");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+
+        sortKeys.put("id", Order.ASCENDING);
+        queryProvider.setSortKeys(sortKeys);
+        reader.setQueryProvider(queryProvider);
+
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("firstname", "C%");
+
+        reader.setParameterValues(parameters);
+
+        return reader;
+    }
+
+    @Bean
+    public ItemProcessor<? super Customer, ? extends Customer2> customItemProcessor() {
+        return new CustomItemProcessor();
+    }
+
+    @Bean
+    public ItemWriter<? super Customer2> customItemWriter() {
+        return new JpaItemWriterBuilder<Customer2>()
+            .usePersist(false)   // default: true
+            .entityManagerFactory(entityManagerFactory)
+            .build();
+    }
+}
