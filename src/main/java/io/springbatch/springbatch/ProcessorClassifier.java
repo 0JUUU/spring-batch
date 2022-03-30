97196812c3374e0bb543fd59b8af06546730c04d
0JUUU <clleo97@naver.com>
2022-03-30 11:02:53 +0900

    feat: :sparkles: 최종적인 ItemProcessor 반환하는 ProcessorClassifier 구현 #14

---
+++ src/main/java/io/springbatch/springbatch/ProcessorClassifier.java
@@ -0,0 +1,20 @@
+package io.springbatch.springbatch;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.classify.Classifier;
+
+public class ProcessorClassifier<C, T> implements Classifier<C, T> {
+
+    private Map<Integer, ItemProcessor<ProcessorInfo, ProcessorInfo>> processorMap = new HashMap<>();
+
+    @Override
+    public T classify(C classifiable) {
+        return (T)processorMap.get(((ProcessorInfo)classifiable).getId() );
+    }
+
+    public void setProcessorMap(Map<Integer, ItemProcessor<ProcessorInfo, ProcessorInfo>> processorMap) {
+        this.processorMap = processorMap;
+    }
+}
