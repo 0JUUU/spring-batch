ea7c6e00141575369d378ac190d7ed1ba720232e
0JUUU <clleo97@naver.com>
2022-03-30 17:59:34 +0900

    feat: :sparkles: Customer 도메인 클래스 구현 #15

---
+++ src/main/java/io/springbatch/springbatch/template/Customer.java
@@ -0,0 +1,11 @@
+package io.springbatch.springbatch.template;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+
+    private String item;
+}
