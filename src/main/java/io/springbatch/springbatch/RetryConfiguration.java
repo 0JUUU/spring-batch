e33b36296656c81763301004f3d57f2b3437ebb0
0JUUU <clleo97@naver.com>
2022-03-30 17:21:54 +0900

    feat: :sparkles: Retry 동작 확인을 위한 Configuration 생성 #15
    - step1에 retry 관련 설정

---
+++ src/main/java/io/springbatch/springbatch/RetryConfiguration.java
@@ -0,0 +1,57 @@
+package io.springbatch.springbatch;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class RetryConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() throws Exception {
+        return jobBuilderFactory.get("batchJob")
+            .incrementer(new RunIdIncrementer())
+            .start(step1())
+            .build();
+    }
+
+    @Bean
+    public Step step1() throws Exception {
+        return stepBuilderFactory.get("step1")
+            .<String, String>chunk(5)
+            .reader(reader())
+            .processor(processor())
+            .writer(items -> items.forEach(System.out::println))
+            .faultTolerant()
+            .retry(RetryableException.class)
+            .retryLimit(2)
+            .build();
+    }
+
+    @Bean
+    public ItemProcessor<? super String, String> processor() {
+        return new RetryItemProcessor();
+    }
+
+    @Bean
+    public ListItemReader<String> reader() {
+        List<String> items = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            items.add(String.valueOf(i));
+        }
+        return new ListItemReader<>(items);
+    }
+}
