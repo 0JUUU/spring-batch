b676b74a1e59e8e570801c5162139b5926ad9e02
0JUUU <clleo97@naver.com>
2022-03-30 11:09:31 +0900

    feat: :sparkles: ClassifierCompositeItemProcessor 동작 확인을 위한 코드 #14

--- src/main/java/io/springbatch/springbatch/ClassifierConfiguration.java
+++ src/main/java/io/springbatch/springbatch/ClassifierConfiguration.java
@@ -1,15 +1,19 @@
 package io.springbatch.springbatch;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.batch.item.support.ClassifierCompositeItemProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -43,7 +47,26 @@ public class ClassifierConfiguration {
                     return i > 3 ? null : processorInfo;
                 }
             })
+            .processor(customItemProcessor())
             .writer(System.out::println)
             .build();
+    }
+
+    @Bean
+    public ItemProcessor<? super ProcessorInfo, ? extends ProcessorInfo> customItemProcessor() {
+        ClassifierCompositeItemProcessor<ProcessorInfo, ProcessorInfo> processor = new ClassifierCompositeItemProcessor<>();
+
+        // 누구를 실행할지 선택하는 역할
+        ProcessorClassifier<ProcessorInfo, ItemProcessor<?, ? extends ProcessorInfo>> classifier = new ProcessorClassifier<>();
+
+        Map<Integer, ItemProcessor<ProcessorInfo, ProcessorInfo>> processorMap = new HashMap<>();
+        processorMap.put(1, new CustomItemProcessor1());
+        processorMap.put(2, new CustomItemProcessor2());
+        processorMap.put(3, new CustomItemProcessor3());
+        classifier.setProcessorMap(processorMap);
+
+        processor.setClassifier(classifier);
+        return processor;
+
     }
 }
