16114746173d988a13a4d773358c56265995fbc3
0JUUU <clleo97@naver.com>
2022-03-31 10:08:07 +0900

    feat: :sparkles: 기본 코드 구현 #16
    - Job의 실행시간 측정하는 StopWatchJobListener
    - Job에 대한 설정하는 ParallelStepConfiguration

---
+++ src/main/java/io/springbatch/springbatch/ParallelStepConfiguration.java
@@ -0,0 +1,42 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@RequiredArgsConstructor
+@Configuration
+public class ParallelStepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("batchJob")
+            .incrementer(new RunIdIncrementer())
+            .start(new TaskletStep())
+            .listener(new StopWatchJobListener())
+            .build();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setThreadNamePrefix("async-thread-");
+        return executor;
+    }
+}
+
