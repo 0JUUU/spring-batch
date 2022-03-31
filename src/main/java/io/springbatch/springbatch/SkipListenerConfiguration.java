1fa1e9c091ef57dd9a05591d4e451722d775655d
0JUUU <clleo97@naver.com>
2022-04-01 02:08:23 +0900

    feat: :sparkles: SkipListener 확인을 위한 Job 구성 #17

---
+++ src/main/java/io/springbatch/springbatch/SkipListenerConfiguration.java
@@ -0,0 +1,70 @@
+package io.springbatch.springbatch;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class SkipListenerConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() throws Exception {
+        return jobBuilderFactory.get("batchJob")
+            .incrementer(new RunIdIncrementer())
+            .start(step1())
+            .build();
+    }
+
+    @Bean
+    public Step step1() throws Exception {
+        return stepBuilderFactory.get("step1")
+            .<Integer, String>chunk(10)
+            .reader(listItemReader())
+            .processor(new ItemProcessor<Integer, String>() {
+                @Override
+                public String process(Integer item) throws Exception {
+                    if (item == 4) {
+                        throw new CustomSkipException("process skipped");
+                    }
+                    System.out.println("process : " + item);
+                    return "item" + item;
+                }
+            })
+            .writer(new ItemWriter<String>() {
+                @Override
+                public void write(List<? extends String> items) throws Exception {
+                    for (String item : items) {
+                        if (item.equals("item5")) {
+                            throw new CustomSkipException("write skipped");
+                        }
+                        System.out.println("write : " + item);
+                    }
+                }
+            })
+            .faultTolerant()
+            .skip(CustomSkipException.class)
+            .skipLimit(2)
+            .listener(new CustomSkipListener())
+            .build();
+    }
+
+    @Bean
+    public ItemReader<Integer> listItemReader() {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        return new LinkedListItemReader<>(list);
+    }
+}
