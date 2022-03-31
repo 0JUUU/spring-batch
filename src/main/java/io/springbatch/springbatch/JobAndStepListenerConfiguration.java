e622a35208abcddb3c8d3e12511e8be61cb905d4
0JUUU <clleo97@naver.com>
2022-04-01 01:12:33 +0900

    feat: :sparkles: 인터페이스 방식으로 구현한 JobExecutionListener 사용 #17
    - stepExecutionListener는 전역적으로 빈 등록을 해 사용

---
+++ src/main/java/io/springbatch/springbatch/JobAndStepListenerConfiguration.java
@@ -0,0 +1,64 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+@Slf4j
+public class JobAndStepListenerConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final CustomStepExecutionListener customStepExecutionListener;
+
+    @Bean
+    public Job job() throws Exception {
+        return jobBuilderFactory.get("batchJob")
+            .incrementer(new RunIdIncrementer())
+            .start(step1())
+            .next(step2())
+            .listener(new CustomJobExecutionListener())
+//            .listener(new CustomAnnotationJobExecutionListener())
+            .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+            .tasklet(new Tasklet() {
+                @Override
+                public RepeatStatus execute(
+                    StepContribution contribution, ChunkContext chunkContext) throws Exception {
+//                        throw new RuntimeException("failed");
+                    return RepeatStatus.FINISHED;
+                }
+            })
+            .listener(customStepExecutionListener)
+            .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+            .tasklet(new Tasklet() {
+                @Override
+                public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                    return RepeatStatus.FINISHED;
+                }
+            })
+            .listener(customStepExecutionListener)
+            .build();
+    }
+}
