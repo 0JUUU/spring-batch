8a8b14d1520612dba1af8aac65b8da7cce0f73c9
0JUUU <clleo97@naver.com>
2022-04-01 01:31:21 +0900

    feat: :sparkles: 어노테이션 방식으로 구현된 CustomChunkListener #17

---
+++ src/main/java/io/springbatch/springbatch/CustomChunkListener.java
@@ -0,0 +1,34 @@
+package io.springbatch.springbatch;
+
+import org.springframework.batch.core.annotation.AfterChunk;
+import org.springframework.batch.core.annotation.AfterChunkError;
+import org.springframework.batch.core.annotation.BeforeChunk;
+import org.springframework.batch.core.scope.context.ChunkContext;
+
+public class CustomChunkListener {
+
+    private int i; // 공유 데이터
+
+    @BeforeChunk
+    public void beforeChunk(ChunkContext context) {
+        System.out.println(">> Before the chunk : "+ context.getStepContext().getStepExecution().getStepName());
+        Object count = context.getStepContext().getStepExecution().getExecutionContext().get("count");
+        if(count == null){
+            context.getStepContext().getStepExecution().getExecutionContext().putInt("count", ++i);
+        }
+    }
+
+    @AfterChunk
+    public void afterChunk(ChunkContext context) {
+        System.out.println(">> After the chunk : "+ context.getStepContext().getStepExecution().getStepName());
+        int count = (int)context.getStepContext().getStepExecution().getExecutionContext().get("count");
+        System.out.println(">> count : "+ count);
+    }
+
+    @AfterChunkError
+    public void afterChunkError(ChunkContext context) {
+        System.out.println(">> After the chunk : "+ context.getStepContext().getStepExecution().getStepName());
+        int count = (int)context.getStepContext().getStepExecution().getExecutionContext().get("count");
+        System.out.println(">> count : "+ count);
+    }
+}
