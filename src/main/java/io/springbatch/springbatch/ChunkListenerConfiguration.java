c069d8840af4fd4523b1037b102fdc396f7bd1ec
0JUUU <clleo97@naver.com>
2022-04-01 01:19:38 +0900

    feat: :sparkles: Chunk 관련 Listener 동작을 알기 위한 간단한 Job 구성 #17

---
+++ src/main/java/io/springbatch/springbatch/ChunkListenerConfiguration.java
@@ -0,0 +1,52 @@
+package io.springbatch.springbatch;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class ChunkListenerConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() throws Exception {
+        return jobBuilderFactory.get("batchJob")
+            .incrementer(new RunIdIncrementer())
+            .start(step1())
+            .build();
+    }
+
+    @Bean
+    public Step step1() throws Exception {
+        return stepBuilderFactory.get("step1")
+            .<Integer, String>chunk(10)
+            .reader(listItemReader())
+            .processor((ItemProcessor) item -> {
+                    return "item" + item;
+            })
+            .writer((ItemWriter<String>) items -> {
+                System.out.println("items = " + items);
+            })
+            .build();
+    }
+
+    @Bean
+    public ItemReader<Integer> listItemReader() {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        return new ListItemReader<>(list);
+    }
+}
