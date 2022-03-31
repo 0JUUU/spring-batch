404bd22191bbe451ce5f977e4c4da07bd6e38ee6
0JUUU <clleo97@naver.com>
2022-04-01 01:11:29 +0900

    feat: :sparkles: 빈으로 사용하기 위해 @Component 어노테이션 추가 #17

--- src/main/java/io/springbatch/springbatch/CustomStepExecutionListener.java
+++ src/main/java/io/springbatch/springbatch/CustomStepExecutionListener.java
@@ -4,8 +4,9 @@ import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomStepExecutionListener implements StepExecutionListener {
 
     @Override
