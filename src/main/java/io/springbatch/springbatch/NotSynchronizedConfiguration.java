b1ea197801e649e2ed941ca572393e3666a81a15
0JUUU <clleo97@naver.com>
2022-03-31 15:51:56 +0900

    feat: :sparkles: Thread에 안전하지 않은 상황 구현 #16
    - Cursor 관련 중복 에러

---
+++ src/main/java/io/springbatch/springbatch/NotSynchronizedConfiguration.java
@@ -0,0 +1,99 @@
+package io.springbatch.springbatch;
+
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ItemReadListener;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@RequiredArgsConstructor
+@Configuration
+@Slf4j
+public class NotSynchronizedConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+    @Bean
+    public Job job() throws Exception {
+        return jobBuilderFactory.get("batchJob")
+            .incrementer(new RunIdIncrementer())
+            .start(step1())
+            .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+            .<Customer, Customer>chunk(100)
+            .reader(customItemReader())
+            .listener(new ItemReadListener<Customer>() {
+                @Override
+                public void beforeRead() {
+
+                }
+
+                @Override
+                public void afterRead(Customer item) {
+                    System.out.println("Thread : " + Thread.currentThread().getName() + " >> item.getId() = " + item.getId());
+                }
+
+                @Override
+                public void onReadError(Exception ex) {
+
+                }
+            })
+            .writer(customerItemWriter())
+            .taskExecutor(taskExecutor())
+            .build();
+    }
+
+    @Bean
+    @StepScope
+    public JdbcCursorItemReader<Customer> customItemReader() {
+        return new JdbcCursorItemReaderBuilder<Customer>()
+            .fetchSize(100)
+            .dataSource(dataSource)
+            .rowMapper(new BeanPropertyRowMapper<>(Customer.class))
+            .sql("select id, firstName, lastName, birthdate from customer")
+            .name("NotSafetyReader")
+            .build();
+    }
+
+    @Bean
+    @StepScope
+    public JdbcBatchItemWriter<Customer> customerItemWriter() {
+        JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
+
+        itemWriter.setDataSource(this.dataSource);
+        itemWriter.setSql("insert into customer2 values (:id, :firstName, :lastName, :birthdate)");
+        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider());
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(8);
+        executor.setThreadNamePrefix("not-safety-thread-");
+        return executor;
+    }
+}
