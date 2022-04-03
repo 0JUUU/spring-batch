006b1330d323d89aaf7219ec013dfd22c0c63c27
0JUUU <clleo97@naver.com>
2022-04-04 02:34:37 +0900

    feat: :sparkles: File에 있는 내용을 DB Product 테이블에 저장하는 Job 구현 #19

---
+++ src/main/java/io/springbatch/springbatch/batch/job/file/FileJobConfiguration.java
@@ -0,0 +1,77 @@
+package io.springbatch.springbatch.batch.job.file;
+
+import io.springbatch.springbatch.batch.chunk.processor.FileItemProcessor;
+import io.springbatch.springbatch.batch.domain.Product;
+import io.springbatch.springbatch.batch.domain.ProductVo;
+import javax.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@RequiredArgsConstructor
+@Configuration
+public class FileJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Bean
+    public Job fileJob() {
+        return jobBuilderFactory.get("fileJob")
+            .start(fileStep())
+            .build();
+    }
+
+    @Bean
+    public Step fileStep() {
+        return stepBuilderFactory.get("fileStep")
+            .<ProductVo, Product>chunk(10)
+            .reader(fileItemReader(null))
+            .processor(fileItemProcessor())
+            .writer(fileItemWriter())
+            .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<ProductVo> fileItemReader(@Value("#{jobParameters['requestDate']}") String requestDate) {
+        return new FlatFileItemReaderBuilder<ProductVo>()
+            .name("flatFile")
+            .resource(new ClassPathResource("product_" + requestDate + ".csv"))
+            .fieldSetMapper(new BeanWrapperFieldSetMapper<>())
+            .targetType(ProductVo.class)
+            .linesToSkip(1)
+            .delimited().delimiter(",")
+            .names("id", "name", "price", "type")
+            .build();
+    }
+
+    @Bean
+    public ItemProcessor<ProductVo, Product> fileItemProcessor() {
+        return new FileItemProcessor();
+    }
+
+    @Bean
+    public ItemWriter<Product> fileItemWriter() {
+        return new JpaItemWriterBuilder<Product>()
+            .entityManagerFactory(entityManagerFactory)
+            .usePersist(true)
+            .build();
+    }
+}
