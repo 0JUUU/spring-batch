3cb0b07ae3c2437d453dc9d8a3628b8a67dea237
0JUUU <clleo97@naver.com>
2022-04-04 18:30:24 +0900

    feat: :sparkles: API 요청 시 들어오는 정보를 매핑할 클래스 구현 #19

---
+++ src/main/java/io/springbatch/springbatch/batch/domain/ApiRequestVO.java
@@ -0,0 +1,12 @@
+package io.springbatch.springbatch.batch.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ApiRequestVO {
+
+    private long id;
+    private ProductVO productVO;
+}
