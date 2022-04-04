0c04dd837c5c97adddbd266d2847f23b12de8f10
0JUUU <clleo97@naver.com>
2022-04-04 20:46:40 +0900

    feat: :sparkles: 실행시간주기 단축 #19
    - 불필요한 import문 제거
    - 30초 -> 10초

--- src/main/java/io/springbatch/springbatch/schedular/ApiJobRunner.java
+++ src/main/java/io/springbatch/springbatch/schedular/ApiJobRunner.java
@@ -1,19 +1,12 @@
 package io.springbatch.springbatch.schedular;
 
-import static org.quartz.JobBuilder.newJob;
-
 import java.util.HashMap;
-import java.util.Map;
-import org.quartz.CronScheduleBuilder;
-import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,7 +18,7 @@ public class ApiJobRunner extends JobRunner {
     @Override
     protected void doRun(ApplicationArguments args) {
         JobDetail jobDetail = buildJobDetail(ApiSchJob.class, "apiJob", "batch", new HashMap());
-        Trigger trigger = buildJobTrigger("0/30 * * * * ?");    // 초 분 시 일 주 월 => 30초마다 API 호출되도록
+        Trigger trigger = buildJobTrigger("0/10 * * * * ?");    // 초 분 시 일 주 월 => 30초마다 API 호출되도록
 
         try {
             scheduler.scheduleJob(jobDetail, trigger);
