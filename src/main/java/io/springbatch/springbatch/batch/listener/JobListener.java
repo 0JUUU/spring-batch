e5131325447c7192d1a88acb35340cd9bfd7f3e8
0JUUU <clleo97@naver.com>
2022-04-04 18:19:40 +0900

    feat: :sparkles: Job 총 소요시간 계산하는 이벤트 리스너 추가 #19

---
+++ src/main/java/io/springbatch/springbatch/batch/listener/JobListener.java
@@ -0,0 +1,18 @@
+package io.springbatch.springbatch.batch.listener;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+
+public class JobListener implements JobExecutionListener {
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        long time = jobExecution.getEndTime().getTime() - jobExecution.getStartTime().getTime();
+        System.out.println(">> ToTal time = " + time);
+    }
+}
