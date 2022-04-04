bdde5514be4a2b091afbf78dbd02e87fc7ec5dd9
0JUUU <clleo97@naver.com>
2022-04-04 18:34:47 +0900

    feat: :sparkles: API 관련 Job 내부에서 동작할 Writer 클래스 구현 #19

---
+++ src/main/java/io/springbatch/springbatch/batch/chunk/writer/ApiItemWriter2.java
@@ -0,0 +1,24 @@
+package io.springbatch.springbatch.batch.chunk.writer;
+
+import io.springbatch.springbatch.batch.domain.ApiRequestVO;
+import io.springbatch.springbatch.batch.domain.ApiResponseVO;
+import io.springbatch.springbatch.service.AbstractApiService;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemWriter;
+
+@Slf4j
+public class ApiItemWriter2 implements ItemWriter<ApiRequestVO> {
+
+    private final AbstractApiService apiService;
+
+    public ApiItemWriter2(AbstractApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    @Override
+    public void write(List<? extends ApiRequestVO> items) throws Exception {
+        ApiResponseVO responseVo = apiService.service(items);
+        System.out.println("responseVo = " + responseVo);
+    }
+}
