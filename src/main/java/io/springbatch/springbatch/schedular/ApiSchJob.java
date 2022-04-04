c4fe7fd935dd8f3ef59323b22d0f85098abd3485
0JUUU <clleo97@naver.com>
2022-04-04 20:11:38 +0900

    feat: :sparkles: Api 관련 Job 설정 및 초기화하는 ApiJobRunner, ApiSchJob 구현 #19

---
+++ src/main/java/io/springbatch/springbatch/schedular/ApiSchJob.java
@@ -0,0 +1,33 @@
+package io.springbatch.springbatch.schedular;
+
+import java.util.Date;
+import lombok.SneakyThrows;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApiSchJob extends QuartzJobBean {
+
+    @Autowired
+    private Job apiJob;
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @SneakyThrows
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        JobParameters jobParameters = new JobParametersBuilder()
+            .addLong("id", new Date().getTime())
+            .toJobParameters();
+
+        jobLauncher.run(apiJob, jobParameters);
+    }
+}
